e9d2961d01b9b2df1e4f6789eeca2b5b1324d106
Kolosov911 <adam_escobaro@mail.ru>
2026-03-09 19:10:49 +0300

    Add *.cs-files

---
+++ New_Class2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KolosovGV
+{
+    internal class New_Class2
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace KolosovGV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        //Kolosov Gleb Am-1-25
+        static public double Sqr(double x)
+        { 
+           return x;
+        }
+    }
+}
